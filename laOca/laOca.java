e36d525529e0079c927428344ddbebb684bb0f91
n0ss4 <n0ss4@protonmail.com>
2018-05-08 13:54:24 +0200

    Upgrade

--- laOca/laOca.java
+++ laOca/laOca.java
@@ -1,150 +1,28 @@
-import java.util.Random;
+package com.proyectoLosDeDelante;
+
+//import java.util.Random;
+
+import acm.graphics.GImage;
+import acm.program.GraphicsProgram;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Main {
 
     public static void main(String[] args) {
-        Random rand = new Random();
-        int turnos = 0;
-        int x = 0;
-        int i = 1;
-        int resta = 0;
-        boolean salir = true;
-        while (salir == true) {
-            if (turnos > 0) {
-                turnos--;
-            }
-            else if (i == 63){
-                salir = false;
-            }
-            else if (i > 63){
-                resta = i - 63;
-                i = 63 - resta;
-            }else {
-                x = rand.nextInt(6) + 1;
-                i += x;
-                boolean oca = true;
-                boolean puente = true;
-                switch (i) {
-                    case 5:
-                        if (oca == true) {
-                            i = i + 9;
-                            oca = false;
-                        } else oca = true;
-                        break; //Oca
-                    case 6:
-                        if (puente == true) {
-                            i = 12;
-                            puente = false;
-                        } else puente = true;
-                        break; //Puente.
-                    case 9:
-                        if (oca == true) {
-                            i = i + 9;
-                            oca = false;
-                        } else oca = true;
-                        break; //Oca
-                    case 12:
-                        if (puente == true) {
-                            i = 6;
-                            puente = false;
-                        } else puente = true;
-                        break; //Puente
-                    case 14:
-                        if (oca == true) {
-                            i = i + 9;
-                            oca = false;
-                        } else oca = true;
-                        break; //Oca
-                    case 18:
-                        if (oca == true) {
-                            i = i + 9;
-                            oca = false;
-                        } else oca = true;
-                        break; //Oca
-                    case 19:
-                        turnos += 2;
-                        break; //Posada
-                    case 23:
-                        if (oca == true) {
-                            i = i + 9;
-                            oca = false;
-                        } else oca = true;
-                        break; //Oca
-                    case 26:
-                        x = rand.nextInt(6)+1;
-                        i += x;
-                        break; //Dados
-                    case 27:
-                        if (oca == true) {
-                            i = i + 9;
-                            oca = false;
-                        } else oca = true;
-                        break; //Oca
-                    case 31:
-                        turnos += 2;
-                        break; //Pozo
-                    case 32:
-                        if (oca == true) {
-                            i = i + 9;
-                            oca = false;
-                        } else oca = true;
-                        break; //Oca
-                    case 36:
-                        if (oca == true) {
-                            i = i + 9;
-                            oca = false;
-                        } else oca = true;
-                        break; //Oca
-                    case 41:
-                        if (oca == true) {
-                            i = i + 9;
-                            oca = false;
-                        } else oca = true;
-                        break; //Oca
-                    case 42:
-                        i = 30;
-                        break; //Laberinto
-                    case 45:
-                        if (oca == true) {
-                            i = i + 9;
-                            oca = false;
-                        } else oca = true;
-                        break; //Oca
-                    case 50:
-                        if (oca == true) {
-                            i = i + 9;
-                            oca = false;
-                        } else oca = true;
-                        break; //Oca
-                    case 52:
-                        turnos += 3;
-                        break; //Carcel
-                    case 53:
-                        x = rand.nextInt(6)+1;
-                        i += x;
-                        break; //Dados
-                    case 54:
-                        if (oca == true) {
-                            i = i + 9;
-                            oca = false;
-                        } else oca = true;
-                        break; //Oca
-                    case 58:
-                        i = 1;
-                        break; //Calavera
-                    case 59:
-                        if (oca == true) {
-                            i = i + 9;
-                            oca = false;
-                        } else oca = true;
-                        break; //Oca
-                    /*case 63:
-                        break; // Meta / Oca*/
-                    default:
-                        break;
+        new com.proyectoLosDeDelante.Main.HelloGraphics().start(args);
+    }
+    public static class HelloGraphics extends GraphicsProgram {
+        public void run() {
+            GImage rect = new GImage("..\\imagenes\\dados\\d1.jpg");
+            add(rect);
+            rect.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent arg0) {
+                    // TODO Auto-generated method stub
                 }
-                System.out.println(i);
-            }
+            });
         }
     }
 }
